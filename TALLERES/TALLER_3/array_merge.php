6acd77c303459e57420f7356e6b938096bcc32f5
WarryTex <emilyyassiel25@gmail.com>
2025-08-31 23:00:54 -0500

    "Añadido ejercicio de array_merge()"

---
+++ TALLERES/TALLER_3/array_merge.php
@@ -0,0 +1,56 @@
+
+<?php
+// Ejemplo básico de array_merge()
+$frutas1 = ["manzana", "pera"];
+$frutas2 = ["naranja", "uva"];
+$todasLasFrutas = array_merge($frutas1, $frutas2);
+echo "Todas las frutas:</br>";
+print_r($todasLasFrutas);
+
+// Ejemplo con arrays asociativos
+$infoPersona1 = ["nombre" => "Juan", "edad" => 30];
+$infoPersona2 = ["ciudad" => "Madrid", "profesion" => "Ingeniero"];
+$infoCompleta = array_merge($infoPersona1, $infoPersona2);
+echo "</br>Información completa de la persona:</br>";
+print_r($infoCompleta);
+
+// Ejercicio: Crea dos arrays, uno con tus comidas favoritas y otro con las de un amigo
+// Usa array_merge() para combinarlos en un solo array de comidas favoritas
+$misComidas = ["Tamal", "Arroz con puerco", "Arroz con pollo"]; // Reemplaza con tus comidas favoritas
+$comidasAmigo = ["Saus", "Sopa china", "Macarrones"]; // Reemplaza con las comidas favoritas de tu amigo
+$todasLasComidas = array_merge($misComidas, $comidasAmigo);
+echo "</br>Todas las comidas favoritas:</br>";
+print_r($todasLasComidas);
+
+// Bonus: Manejo de claves duplicadas
+$array1 = ["a" => "rojo", "b" => "verde","c"=> "morado"];
+$array2 = ["b" => "azul", "c" => "amarillo"];
+$resultadoMerge = array_merge($array1, $array2);
+echo "</br>Resultado de merge con claves duplicadas:</br>";
+print_r($resultadoMerge);
+
+// Extra: Uso de array_merge() con múltiples arrays
+$numeros1 = [1, 2, 3];
+$numeros2 = [4, 5, 6];
+$numeros3 = [7, 8, 9];
+$todosLosNumeros = array_merge($numeros1, $numeros2, $numeros3);
+echo "</br>Todos los números combinados:</br>";
+print_r($todosLosNumeros);
+
+//Practica
+// Extra: Uso de array_merge() con múltiples arrays
+$numeros4 = [10, 11, 12];
+$numeros5 = [13, 14, 15];
+$numeros6 = [16, 17, 18];
+$todosLosNumeros = array_merge($numeros4, $numeros5, $numeros6);
+echo "</br>Todos los números combinados:</br>";
+print_r($todosLosNumeros);
+
+// Desafío: Usa array_merge() junto con array_unique() para combinar arrays y eliminar duplicados
+$colores1 = ["rojo", "verde", "azul"];
+$colores2 = ["verde", "amarillo", "rojo"];
+$coloresUnicos = array_unique(array_merge($colores1, $colores2));
+echo "</br>Colores únicos después de combinar:</br>";
+print_r($coloresUnicos);
+?>
+      
